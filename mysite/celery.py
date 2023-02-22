dd886500e70afbc294be3e295bfcda6a0e700882
abraham <fcoabrahamprz@gmail.com>
2023-02-22 15:36:02 -0700

    added variable to project name

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,13 +1,13 @@
-from __future__ import absolute_import, unicode_literals
-
 import os
 
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+PROJECT = 'mysite'
 
-app = Celery('mysite')
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'{PROJECT}.settings')
+
+app = Celery(PROJECT)
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
@@ -15,10 +15,10 @@ app = Celery('mysite')
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django app configs.
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 
-@app.task(bind=True)
+@app.task(bind=True, ignore_result=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    print(f'Request: {self.request!r}')
